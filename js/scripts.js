55f4b154dcb6f4732d464de18dfd441b62125819
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-09-30 17:15:33 +0200

    fun with JS

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Komentarz liniowy.
+
+var name = prompt('Wpisz swoje imię');
+var surname = prompt('Wpisz swoje nazwisko');
+alert('Witaj, ' + name + ' ' + surname);
+console.log('Witaj, ' + name + ' ' + surname);
+
