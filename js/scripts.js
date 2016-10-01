734b8bb616601a0455f01f8de2c84afd9f0a4d25
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-01 13:12:39 +0200

    nothing new

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,17 @@ var
 a = 5,
 h = 10,
 triangleArea = a * h / 2;
-/*
+
 name = prompt('Wpisz swoje imię'),
 surname = prompt('Wpisz swoje nazwisko'),
 foo = ['1', 2, 3.4],
 points = 1,
 x = 99;
 
-alert('Witaj, ' + name + ' ' + surname + foo);
+alert('Witaj, ' + name + ' ' + surname +  ' ' + foo);
+
 console.log('Witaj, ' + name + ' ' + surname);
-*/
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ', wynosi: ' + triangleArea);
-/*
 console.log( 'bieżąca wartość zmiennej points to: ', points ); // 1
 console.log( 'bieżąca wartość zmiennej points to: ', points++ ); // 1
 console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
@@ -24,7 +23,7 @@ console.log( 'bieżąca wartość zmiennej points to: ', points ); // 3
 console.log( 'bieżąca wartość zmiennej points to: ', points-- ); // 3
 console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
 console.log(  x % 3);
-
+/*
 var person = {
     name: 'Jon',
     surname: 'Snow',
