27fbadb0b0aa3d8d90e2fb58dfddd6e7f0a4b19b
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-01 12:55:20 +0200

    Triangle Area

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,40 @@
 // Komentarz liniowy.
 
-var name = prompt('Wpisz swoje imię');
-var surname = prompt('Wpisz swoje nazwisko');
-alert('Witaj, ' + name + ' ' + surname);
-console.log('Witaj, ' + name + ' ' + surname);
+var 
+a = 5,
+h = 10,
+triangleArea = a * h / 2;
+/*
+name = prompt('Wpisz swoje imię'),
+surname = prompt('Wpisz swoje nazwisko'),
+foo = ['1', 2, 3.4],
+points = 1,
+x = 99;
 
+alert('Witaj, ' + name + ' ' + surname + foo);
+console.log('Witaj, ' + name + ' ' + surname);
+*/
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ', wynosi: ' + triangleArea);
+/*
+console.log( 'bieżąca wartość zmiennej points to: ', points ); // 1
+console.log( 'bieżąca wartość zmiennej points to: ', points++ ); // 1
+console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
+console.log( 'bieżąca wartość zmiennej points to: ', ++points ); // 3
+console.log( 'bieżąca wartość zmiennej points to: ', points ); // 3
+console.log( 'bieżąca wartość zmiennej points to: ', points-- ); // 3
+console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
+console.log(  x % 3);
+
+var person = {
+    name: 'Jon',
+    surname: 'Snow',
+    siblings: 5,
+    knownThings: ['Nothing','Night Watch']
+};
+
+var userName = person.name;
+var userSurname = person['surname'];
+
+alert('Witaj, ' + userName + ' ' + userSurname);
+console.log('Witaj, ' + userName + ' ' + userSurname);
+*/
