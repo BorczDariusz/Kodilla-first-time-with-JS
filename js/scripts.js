44cf08f37c76ce834e5baeb8fb5f08bc53c440ed
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-01 20:21:09 +0200

    zadanie 8_5

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,65 @@
-// Komentarz liniowy.
-
 var 
 a = 5,
-h = 10,
-triangleArea = a * h / 2;
+b = 10,
+value = (a * a) + (2 * a * b) - (b * b);
 
-name = prompt('Wpisz swoje imię'),
-surname = prompt('Wpisz swoje nazwisko'),
-foo = ['1', 2, 3.4],
-points = 1,
-x = 99;
+console.log( value );
 
-alert('Witaj, ' + name + ' ' + surname +  ' ' + foo);
+if (value < 0) {
+	valueResult = 'wynik ujemny'
+}
+if (value > 0) {
+	valueResult = 'wynik dodatni'
+}
 
-console.log('Witaj, ' + name + ' ' + surname);
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ', wynosi: ' + triangleArea);
-console.log( 'bieżąca wartość zmiennej points to: ', points ); // 1
-console.log( 'bieżąca wartość zmiennej points to: ', points++ ); // 1
-console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
-console.log( 'bieżąca wartość zmiennej points to: ', ++points ); // 3
-console.log( 'bieżąca wartość zmiennej points to: ', points ); // 3
-console.log( 'bieżąca wartość zmiennej points to: ', points-- ); // 3
-console.log( 'bieżąca wartość zmiennej points to: ', points ); // 2
-console.log(  x % 3);
-/*
-var person = {
-    name: 'Jon',
-    surname: 'Snow',
-    siblings: 5,
-    knownThings: ['Nothing','Night Watch']
-};
+console.log( valueResult);
 
-var userName = person.name;
-var userSurname = person['surname'];
+if (value >= 0) {
+	console.log( 'Liczba ' + value + ' jest liczbą dodatnią.' ); }
 
-alert('Witaj, ' + userName + ' ' + userSurname);
-console.log('Witaj, ' + userName + ' ' + userSurname);
-*/
+if (value <= 0) {
+	console.log( 'Liczba ' + value + ' jest liczbą ujemną.' ); }
+
+
+if (value = 0) {
+	console.log( 'Wynik jest równy 0'); }
+else {
+	console.log( 'Wynik nie jest równy 0'); }
+
+// Po za zadaniem
+ 
+if ( !(value = 25)) {
+	console.log( value + 'jest równe 25'); }
+else {
+	console.log('OPERATOR NEGACJI: ' + value + ' nie jest równe 25'); }
+
+
+if ( (b < a) && (value > b)) {
+	console.log('ŁĄCZNIK i: ' + b +  ' jest mniejsze niż ' + a + ' , ' + value + ' jest mniejsze niż ' + b); }
+else if ( (b > a) && (value > b)) {
+	console.log('ŁĄCZNIK i:  ' + b + ' jest większe niż ' + a + ' , ' + value + ' jest większe niż ' + b); }
+else {
+	console.log( b + '=' + a);}
+
+
+// switch 
+
+switch(value) {
+  case 1:
+    console.log(value + ' /5= ' + b)
+    break;
+  case 2:
+    console.log(value + ' /5= ' + value)
+    break;
+  case 25:
+    console.log(value + ' / 5 = ' + a)
+  default:
+    console.log(value/10) // default wyświetli się, ponieważ nie dałem ,,break".
+}
+
+// short if
+
+IsValueBiggerThan = value > b ? value + ' jest większe niż ' + b : value + ' jest mniejsze niż ' + b;
+console.log( IsValueBiggerThan );
+
+
